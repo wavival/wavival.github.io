154bdc1b16db6e8dd9462d1dc562364ce979aac2
Valentina Ramírez <wavival.dev@luminaw.co>
2026-04-11 00:50:08 +0000

    [CREATING] --404-route

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://wavival.github.io',
-  base: '/portfolio/',
   vite: {
     resolve: {
       alias: {
